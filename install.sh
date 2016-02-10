b53d93b2e307655618496f4dc6555600887bd7b6
assout <assout@users.noreply.github.com>
2016-02-10 21:22:40 +0900

    Update

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# TODO: チェック処理リファクタ
+if [ "${OSTYPE}" = msys ] ; then echo "install.sh: It doesn't meet the prerequisites" >&2 && exit 1 ; fi
+
 mkdir -p ~/Dropbox/
 cd ~/Dropbox
 git clone git@github.com:assout/memolist.git
