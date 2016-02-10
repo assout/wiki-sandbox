b83b10691c478f40c6e39f8a0a0165ababa5ba4e
assout <assout@users.noreply.github.com>
2016-02-10 21:26:38 +0900

    Update

--- install.bat
+++ install.bat
@@ -1,4 +1,6 @@
-mklink /J "D:\admin\memolist" "D:\admin\Documents\memolist"
+rem 当ファイル自身のパス
+set here=%~dp0
+mklink /J "D:\admin\memolist" "%here%"
 
 git config user.name "assout"
 git config user.email "assout@users.noreply.github.com"
