6663f8f9dea4bd6318f2a0f5fce2302239be3ae0
assout <assout@users.noreply.github.com>
2016-03-30 03:15:06 +0900

    Update symlink

--- symlink.sh
+++ symlink.sh
@@ -4,6 +4,9 @@ readonly CMD_NAME=$(basename "${0}")
 readonly HERE=$(cd "$(dirname "$0")" || exit 1; pwd)
 
 ln -sf "${HERE}/" ~/
+if [ "${OSTYPE}" = "linux-gnu" ] ; then
+  ln -sf "${HERE}/" ~/Dropbox/
+fi
 ln -fL "${HERE}/.mdlrc" ~/
 ln -fL "${HERE}/.mdlrc.style.rb" ~/
 
